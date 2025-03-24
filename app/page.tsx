215441820263f184767b659e2e8888e3c47eb81a
Rodrigo Cabrera <rodrigo.cabrera@infracommerce.lat>
2025-03-24 11:59:44 -0300

    Update: refactor layout structure in RootLayout and Home components for improved organization and styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,14 @@ import ResourcesPosts from "@/app/components/ResourcesPosts";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <section className="px-4 flex justify-between gap-4 w-full">
-          <h1 className="text-3xl font-bold text-gray-900">Resources</h1>
-          <AddPostButton />
-        </section>
-        <section>
-          <ResourcesPosts />
-        </section>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>
-          copyright © {new Date().getFullYear()} by <a href="https://github.com/rodrigaocabrera">Rodrigo cabrera</a>
-        </p>
-      </footer>
-    </div>
+    <>
+      <section className="px-4 flex justify-between gap-4 w-full">
+        <h1 className="text-3xl font-bold text-gray-900">Resources</h1>
+        <AddPostButton />
+      </section>
+      <section>
+        <ResourcesPosts />
+      </section>
+    </>
   );
 }
